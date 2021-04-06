cd96594d5b84589beec9f7296b8c0c1a82c9a2c0
Waseem Akram <waseem07799@gmail.com>
2021-04-06 21:42:12 +0530

    Initial Commit

---
+++ Checkout/CheckoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckoutApp.swift
+//  Checkout
+//
+//  Created by Waseem Akram on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct CheckoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
